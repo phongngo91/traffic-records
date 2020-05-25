2f81a5e89f69ff3f5c544c78a6cdede62ace4e1c
Phong Ngo <phongngo91@gmail.com>
2020-05-25 16:28:30 -0400

    Parsed ordinances for stop signs into database

--- py_utils/ordinance_stop_seed.py
+++ py_utils/ordinance_stop_seed.py
@@ -11,15 +11,35 @@ with open("../data-import/ordinances/tc8-52_stop_intersections.csv","r") as file
     reader = csv.reader(file, delimiter= '|')
     for row in reader:
       if len(row) > 2: 
-        street, direction, cross, type = row[0], row[1], row[2], "stop"
-        streetArr = street.split("-")
+        street, direction, cross, type = row[0], row[1], row[2], "stop sign"
+        streetArr = street.split(" — ", 1)
         mainStreet = streetArr[0]
-        info = streetArr[1]
-        loc = info.split("[")
-        hamlet = loc[0]
-        dates = loc[1].split("by")
-        adoption = dates[0]
-        number = dates[1]
+
+        if len(streetArr) > 1:
+          info = streetArr[1]
+          loc = info.split("[")
+          hamlet = loc[0]
+
+          if len(loc) > 1: 
+            dates = loc[1].split("by")
+            adoption = dates[0]
+            
+            if len(dates) > 1:
+              number = dates[1]
+            else:
+              number = ""
+          else:
+            dates = ""
+            adoption = ""
+            number = ""
+
+        else:
+          info = ""
+          loc = ""
+          hamlet = ""
+          dates = ""
+          adoption = ""
+          number = ""
 
         ordData = {\
         "streetName": mainStreet, "crossStreet": cross, "adoptionDate": adoption,\
