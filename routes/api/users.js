1f6137b8108518e5572b68a0ccecf567649ddef4
Phong Ngo <phongngo91@gmail.com>
2020-05-25 11:02:28 -0400

    create valid text validator

--- routes/api/users.js
+++ routes/api/users.js
@@ -2,6 +2,8 @@ const express = require("express");
 const router = express.Router();
 const User = require("../../models/User");
 const bcrypt = require("bcryptjs");
+const keys = require("../../config/keys");
+const jwt = require("jsonwebtoken");
 
 router.get("/test", (req, res) => {
   res.json({ msg: "This is the users route" });
@@ -39,13 +41,25 @@ router.post("/login", (req, res) => {
   User.findOne({ username })
     .then(user => {
       if (!user){
-        return res.status(404).json({ email: "This user does not exist." });
+        return res.status(404).json({ username: "This user does not exist." });
       }
 
       bcrypt.compare(password, user.password)
         .then( isMatch => {
           if (isMatch){
-            res.json({ msg: "Success"});
+            const payload = {
+              id: user.id,
+              username: user.username
+            };
+            jwt.sign(payload,
+              keys.secretOrKey,
+              { expiresIn: 3600 },
+              (err, token) => {
+                res.json({
+                  success: true,
+                  token: "Bearer " + token
+                });
+              });
           } else {
             return res.status(400).json( {password: "Incorrect password"} );
           }
