ef7dcab80f4c0fb0ca712391b02ffb0c1832c3aa
Phong Ngo <phongngo91@gmail.com>
2020-05-26 13:17:22 -0400

    Add react

--- routes/api/ordinances.js
+++ routes/api/ordinances.js
@@ -1,14 +1,27 @@
 const express = require("express");
 const router = express.Router();
+const passport = require("passport");
+const Ordinance = require("../../models/Ordinance");
 
 router.get("/test", (req, res) => {
   res.json({ msg: "This is the ordinances route" });
 });
 
+router.get("/", (req, res) => {
+  debugger
+
+  Ordinance
+    .find()
+    .then(ordinances => res.json(ordinances))
+    .catch(err => res.status(400).json(err));
+});
+
 router.post("/", (req,res) => {
+  passport.authenticate("jwt", { session: false });
 
   const ordinance = new Ordinance();
-  ordinance.number = "T.O. 12-2013";
+  ordinance.number = req.ordinance.number;
+  ordinance.streetName = req.ordinance.streetName;
 
   ordinance.save()
     .then(action => res.json({ message: "successfully saved"}))
